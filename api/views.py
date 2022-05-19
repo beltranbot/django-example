7e885dc50bf2ad4fcfef7b5c0a9f404f112f56f2
beltranbot <beltranbot@gmail.com>
2022-05-19 10:30:28 -0500

    example of param headers and content_type on request

--- api/views.py
+++ api/views.py
@@ -1,6 +1,20 @@
 from django.http import JsonResponse
+import json
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({
-        "message": "Hi there, this is your Django API response!!"
-    })
+    body = request.body # json data -> bstring of json data
+    print("request.body")
+    print(request.GET)
+    print(body)
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+
+    # data["headers"] = request.headers
+    data["params"] = dict(request.GET)
+    data["headers"] = dict(request.headers)
+    data["content_type"] = request.content_type
+
+    return JsonResponse(data)
