cc32382ae651fa51363045f55dcd78c7452d5184
beltranbot <beltranbot@gmail.com>
2022-05-30 22:12:03 -0500

    URLs, reverse and serializers

--- products/serializers.py
+++ products/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -8,10 +9,19 @@ class ProductSerializer(serializers.ModelSerializer):
         this is known as an inline field
     """
     my_discount = serializers.SerializerMethodField(read_only=True)
+    edit_url = serializers.SerializerMethodField(read_only=True)
+    # recommended method
+    # adds a url to the resource in the json body response
+    url = serializers.HyperlinkedIdentityField(
+        view_name='product-detail',
+        lookup_field='pk'
+    )
 
     class Meta:
         model = Product
         fields = [
+            "edit_url",
+            "url",
             "pk",
             "title",
             "content",
@@ -20,6 +30,15 @@ class ProductSerializer(serializers.ModelSerializer):
             # tells django to look for the my_discount property
             "my_discount"
         ]
+
+    def get_edit_url(self, obj):
+        request = self.context.get('request')
+        if request is None:
+            return None
+        return reverse("product-edit", kwargs={
+            "pk": obj.pk
+        }, request=request)
+
 
     def get_my_discount(self, obj):
         """
