8fc67f501c9b3441f3f040969f3dd162ae3c0e4e
beltranbot <beltranbot@gmail.com>
2022-05-30 20:30:21 -0500

    adding authentication_classes and permission_classes

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, permissions, authentication
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -12,6 +12,8 @@ from django.shortcuts import get_object_or_404
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         # serializer.save(user=self.request.user)
