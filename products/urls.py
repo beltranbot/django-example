3de31fae0867c71f27b3810fa7fa83439af9da65
beltranbot <beltranbot@gmail.com>
2022-05-30 19:39:38 -0500

    example of function based view

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list_create_view),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view())
+    path('<int:pk>/', views.product_detail_view)
+    # path('', views.product_alt_view),
+    # path('<int:pk>/', views.product_alt_view)
 ]
